8f2084fa099eac0b9a905d72aac1a3227936450a
Manohar Kurapati <kvmanohar@gmail.com>
2019-03-03 10:28:01 +0000

    setState to update the state of the component

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,33 @@ import Person from './Person/Person.jsx';
 
 class App extends Component {
 	state = {
-		person:[
+		persons:[
 			{name: 'Max', age: 28},
 			{name: 'Mano', age:29},
 			{name: 'Tester', age: 26}
-		]
+		],
+		otherState: 'Some other value'
 	}
+
+	switchNameHandler = ()=>{
+		this.setState({
+			persons: [
+				{ name: 'Maximilan', age: 28 },
+				{ name: 'Mano', age: 29 },
+				{ name: 'Tester', age: 37 }
+			]
+		})
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<h1>Hi, I'am a React App</h1>
 				<p>this is really working !!!</p>
-				<Person name={this.state.person[0].name} age={this.state.person[0].age}/>
-				<Person name={this.state.person[1].name} age={this.state.person[1].age}> My Hobbies: Coding </Person>
-				<Person name={this.state.person[2].name} age={this.state.person[2].age} />
+				<button onClick={this.switchNameHandler}>Switch Name</button>
+				<Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+				<Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Coding </Person>
+				<Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
 			</div>
 		);
 	}
