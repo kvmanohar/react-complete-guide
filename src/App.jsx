e536918113312caa7f700655b50bcbcd0432c61b
Manohar Kurapati <kvmanohar@gmail.com>
2019-03-03 18:53:27 +0000

    2 way binding of props and changeing of props

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,23 @@ class App extends Component {
 		});
 	};
 
+	nameChangeHandler = (event) => {
+		this.setState({
+			persons: [{ name: 'Max', age: 28 }, { name: event.target.value, age: 29 }, { name: 'Tester', age: 37 }]
+		});
+	};
 	render() {
 		return (
 			<div className="App">
 				<h1>Hi, I'am a React App</h1>
 				<p>this is really working !!!</p>
-				<button onClick={() => this.switchNameHandler('Maximus')}>Switch Name</button>	
+				<button onClick={() => this.switchNameHandler('Maximus')}>Switch Name</button>
 				<Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
 				<Person
 					name={this.state.persons[1].name}
 					age={this.state.persons[1].age}
 					click={this.switchNameHandler.bind(this, 'Max!!!')}
+					changed={this.nameChangeHandler}
 				>
 					My Hobbies: Coding
 				</Person>
