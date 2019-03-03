045ac13defa76384230a9bac633adc05360a303d
Manohar Kurapati <kvmanohar@gmail.com>
2019-03-03 09:49:04 +0000

    Creating state of a class

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import './App.css';
 import Person from './Person/Person.jsx';
 
 class App extends Component {
+	state = {
+		person:[
+			{name: 'Max', age: 28},
+			{name: 'Mano', age:29},
+			{name: 'Tester', age: 26}
+		]
+	}
 	render() {
 		return (
 			<div className="App">
 				<h1>Hi, I'am a React App</h1>
 				<p>this is really working !!!</p>
-				<Person name="Max" age="28"/>
-				<Person name="Mano" age="39"> My Hobbies: Coding </Person> 
+				<Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+				<Person name={this.state.person[1].name} age={this.state.person[1].age}> My Hobbies: Coding </Person>
+				<Person name={this.state.person[2].name} age={this.state.person[2].age} />
 			</div>
 		);
 	}
