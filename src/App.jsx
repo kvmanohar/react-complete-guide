4a2d0ea385d0bcd7ea10365d3b7c9035f7000471
Manohar Kurapati <kvmanohar@gmail.com>
2019-03-03 18:25:17 +0000

    passing functions variables in 2 ways

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,30 @@ import Person from './Person/Person.jsx';
 
 class App extends Component {
 	state = {
-		persons:[
-			{name: 'Max', age: 28},
-			{name: 'Mano', age:29},
-			{name: 'Tester', age: 26}
-		],
+		persons: [{ name: 'Max', age: 28 }, { name: 'Mano', age: 29 }, { name: 'Tester', age: 26 }],
 		otherState: 'Some other value'
-	}
+	};
 
-	switchNameHandler = ()=>{
+	switchNameHandler = (newName) => {
 		this.setState({
-			persons: [
-				{ name: 'Maximilan', age: 28 },
-				{ name: 'Mano', age: 29 },
-				{ name: 'Tester', age: 37 }
-			]
-		})
-	}
+			persons: [{ name: newName, age: 28 }, { name: 'Mano', age: 29 }, { name: 'Tester', age: 37 }]
+		});
+	};
 
 	render() {
 		return (
 			<div className="App">
 				<h1>Hi, I'am a React App</h1>
 				<p>this is really working !!!</p>
-				<button onClick={this.switchNameHandler}>Switch Name</button>
-				<Person 
-					name={this.state.persons[0].name} 
-					age={this.state.persons[0].age}/>
-				<Person 
-					name={this.state.persons[1].name} 
+				<button onClick={() => this.switchNameHandler('Maximus')}>Switch Name</button>	
+				<Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+				<Person
+					name={this.state.persons[1].name}
 					age={this.state.persons[1].age}
-					click={this.switchNameHandler}> My Hobbies: Coding </Person>
+					click={this.switchNameHandler.bind(this, 'Max!!!')}
+				>
+					My Hobbies: Coding
+				</Person>
 				<Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
 			</div>
 		);
