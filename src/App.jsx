aaba85a226582743a21bd5f63b21d422d5ced654
Manohar Kurapati <kvmanohar@gmail.com>
2019-03-03 18:13:07 +0000

    passing functions to components

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,13 @@ class App extends Component {
 				<h1>Hi, I'am a React App</h1>
 				<p>this is really working !!!</p>
 				<button onClick={this.switchNameHandler}>Switch Name</button>
-				<Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-				<Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Coding </Person>
+				<Person 
+					name={this.state.persons[0].name} 
+					age={this.state.persons[0].age}/>
+				<Person 
+					name={this.state.persons[1].name} 
+					age={this.state.persons[1].age}
+					click={this.switchNameHandler}> My Hobbies: Coding </Person>
 				<Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
 			</div>
 		);
