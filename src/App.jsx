8187ea9ece2287d369926f850d1c72108a3464eb
Manohar Kurapati <kvmanohar@gmail.com>
2019-03-04 22:03:44 +0000

    dynamic content using if statment

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,23 @@ class App extends Component {
 			cursor: 'pointer'
 		};
 
+		let persons = null;
+		if (this.state.showPersons) {
+			persons = (
+				<div>
+					<Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+					<Person
+						name={this.state.persons[1].name}
+						age={this.state.persons[1].age}
+						click={this.switchNameHandler.bind(this, 'Max!!!')}
+						changed={this.nameChangeHandler}
+					>
+						My Hobbies: Coding
+					</Person>
+					<Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+				</div>
+			);
+		}
 		return (
 			<div className="App">
 				<h1>Hi, I'am a React App</h1>
@@ -40,20 +57,7 @@ class App extends Component {
 				<button style={buttonStyle} onClick={this.tooglePersonsHandler}>
 					Toggle Persons
 				</button>
-				{this.state.showPersons ? (
-					<div>
-						<Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-						<Person
-							name={this.state.persons[1].name}
-							age={this.state.persons[1].age}
-							click={this.switchNameHandler.bind(this, 'Max!!!')}
-							changed={this.nameChangeHandler}
-						>
-							My Hobbies: Coding
-						</Person>
-						<Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-					</div>
-				) : null}
+				{persons}
 			</div>
 		);
 	}
